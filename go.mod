500df3d7106b2ccb994a6daa8c7a840b06bcc23a
Juan Osorio Robles <Juan.osoriorobles@eu.equinix.com>
2022-06-06 10:42:46 +0300

    helpers: Add log parameter to `OpenAuditLogFileUntilSuccess` (#60)
    The `OpenAuditLogFileUntilSuccess` function would silently block if the
    audit file isn't present. While this behavior is fine, it's not an ideal
    developer experience if folks are not aware of this. So, this PR enables
    one to pass a `logr.Logger` instance in order to print out a message
    that indicates this behavior, thus making things clearer for developers.
    
    If no logger is passed, a default one is created using the zap library.
    
    Signed-off-by: Juan Antonio Osorio <juan.osoriorobles@eu.equinix.com>

--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.8.0
+	github.com/go-logr/logr v1.2.3
+	github.com/go-logr/zapr v1.2.3
 	github.com/google/uuid v1.3.0
 	github.com/prometheus/client_golang v1.12.2
 	github.com/prometheus/common v0.34.0
 	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.7.1
+	go.uber.org/zap v1.21.0
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
 )
 
@@ -35,6 +38,8 @@ require (
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
--- helpers/helpers_test.go
+++ helpers/helpers_test.go
@@ -19,10 +19,14 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
+	"github.com/go-logr/zapr"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
 	"github.com/metal-toolbox/auditevent/helpers"
 )
@@ -87,4 +91,47 @@ func TestOpenAuditLogFileError(t *testing.T) {
 
 	err = os.Remove(tmpfile)
 	require.NoError(t, err)
+}
+
+func TestOpenAuditLogFileWithLogger(t *testing.T) {
+	t.Parallel()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	tmpdir := t.TempDir()
+	tmpfile := filepath.Join(tmpdir, "audit.log")
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Second)
+		fd, err := os.OpenFile(tmpfile, os.O_RDONLY|os.O_CREATE, 0o600)
+		require.NoError(t, err)
+		err = fd.Close()
+		require.NoError(t, err)
+	}()
+
+	var nlogs int32
+
+	z, err := zap.NewDevelopment(zap.Hooks(func(e zapcore.Entry) error {
+		atomic.AddInt32(&nlogs, 1)
+		return nil
+	}))
+	require.NoError(t, err, "failed to create logger")
+	l := zapr.NewLogger(z)
+
+	fd, err := helpers.OpenAuditLogFileUntilSuccess(tmpfile, l)
+	require.NoError(t, err)
+	require.NotNil(t, fd)
+
+	err = fd.Close()
+	require.NoError(t, err)
+
+	// We wait so we don't leak file descriptors
+	wg.Wait()
+
+	err = os.Remove(tmpfile)
+	require.NoError(t, err)
+
+	require.Equal(t, int32(2), nlogs, "expected 2 logs. One for the wait and one for the success.")
 }
